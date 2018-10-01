c2b03d8a94735840faeb7eac03f915ec5f571152
user <Tatjana.Solovjova@rtu.lv>
2018-10-01 11:43:51 +0300

    20181001_11_45

--- mans_skripts.sh
+++ mans_skripts.sh
@@ -2,3 +2,9 @@
 mkdir Mape
 cd Mape
 mkdir MapeMapee
+
+# Skripts (vai komanda) tiek izpildīts, ja tas apraksts
+# ir atrodāms (caur ./, ~/, /home/user/ utt. vai
+# caur $PATH - respektīvi, standarta ceļiem),
+# otrkārt, skripta failam ir jābūt ar izpildīšanas
+# tiesībām
