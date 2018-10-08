60a1198863247a6b1f594f1e08d56835295a5e34
user <Tatjana.Solovjova@rtu.lv>
2018-10-08 11:44:06 +0300

    20181008_11_45

--- dialogs.py
+++ dialogs.py
@@ -6,6 +6,8 @@ a = input("Cien. liet., lūdzu, ievadi skaitli: ")
 # tapēc ievadītā lieluma datu tips vēlāk ir jāparveido
 a = int(a)
 
+# python valoda basltās uz C valodas => print strādā līdzīgi printf
+# http://www.cplusplus.com/reference/cstdio/printf/
 print("Liet., Tu esi iavadījis skaitli: %d"%(a))
 aa = a * a
 print("Tavs skaitlis kvadrātā ir: %d"%(aa))
